a7ab55e5266aae4bf559e17ccc15143514c9058a
MAMahmoudi <mohamed.mahmoudi@univ-mascara.dz>
2022-02-26 15:30:13 +0100

    Removed personnel photos

--- face_detect.py
+++ face_detect.py
@@ -6,7 +6,7 @@ cv.imshow('test', img)
 gray = cv.cvtColor(img,cv.COLOR_RGB2GRAY)
 cv.imshow('Gray', gray)
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
-faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=4)
+faces_rect = haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
 print(f'# of faces found= {len(faces_rect)}')
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),thickness=2)
